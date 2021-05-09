e73a056d7bda0824b37ee04fede60e32d9bdbb33
Onur <onrozcan20@gmail.com>
2021-05-09 12:13:46 +0300

    we did the index page

--- towel_store/urls.py
+++ towel_store/urls.py
@@ -2,15 +2,12 @@ from django.http import HttpResponse
 from django.urls import path, include
 from django.contrib import admin
 
-def index(request):
-    return HttpResponse("Welcome to Towel Store")
-
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
-    path('',index, name='index' ),
+    path('', include('products.urls'))
 
 
 ]
